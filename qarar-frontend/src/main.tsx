a24ce1aaf430c208bea3e7798e246ffc652d8f7f
systemqarar <systemqarar@gmail.com>
2026-07-17 22:21:45 +0000

    fix socket 56

--- qarar-frontend/src/main.tsx
+++ qarar-frontend/src/main.tsx
@@ -1,21 +1,13 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import AppRoutes from './routes/AppRoutes'
 import { AuthProvider } from './context/AuthContext'
-import { SocketProvider } from './context/SocketContext' // 🔌 مسار مطابق تماماً لاسم ملفك الفعلي
+import { SocketProvider } from './context/SocketContext'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    /* 1. طبقة الأمان والتوكن في الأعلى */
-    <AuthProvider>
-      
-      /* 2. طبقة السوكت تحتها مباشرة لتقرأ التوكن فوراً */
-      <SocketProvider>
-        
-        /* 3. مسارات وصفحات نظام قرار بالكامل */
-        <AppRoutes />
-        
-      </SocketProvider>
-      
-    </AuthProvider>
+  <AuthProvider>
+    <SocketProvider>
+      <AppRoutes />
+    </SocketProvider>
+  </AuthProvider>
 )
